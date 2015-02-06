7dd158ea166fc06cf1a092282f14922bc430ab44
jnmclarty <jeffrey.mclarty@gmail.com>
2015-02-05 21:04:36 -0500

    Attempt to fix setup script

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ def read(*paths):
     with open(os.path.join(*paths), 'r') as f:
         return f.read()
 
+v = '0.1rc2'
 print "Setting up smuggle"
 
 setup(
   name = 'smuggle',
-  version = '0.1rc1',
+  version = v,
+  packages = ['smuggle'],
   description = 'Log, catalogue, and move python objects via pickling',
   long_description=(read('README.rst')),
   author = 'Jeffrey McLarty',
   author_email = 'jeffrey.mclarty@gmail.com',
   url = 'https://github.com/jnmclarty/smuggle',
-  download_url = 'https://github.com/jnmclarty/smuggle/tarball/0.1rc1',
-  py_modules=['smuggle'],
-  include_package_data=True,
+  download_url = 'https://github.com/jnmclarty/smuggle/tarball/' + v,
   keywords = ['debugging', 'logging', 'smuggle', 'smuggler', 'exception', 'pickle', 'pickling'],
   classifiers = ['Development Status :: 4 - Beta',
                  'Topic :: Office/Business',
